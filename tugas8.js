bd23e3343883d71439c7a762818a61ca7aaf4e76
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:16:07 +0700

    Tugas 8 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function panggilAku() {
+  var motor = {
+    merk: ['honda','suzuki','yamaha'],
+    tahun: [2018,2019,2020],
+    harga: [18000000,19000000,20000000],
+  }
+  motor.warna = ['hitam','putih','abu'];
+  console.log(motor)
+  console.log(motor.warna[2])
+}
+
+panggilAku()
